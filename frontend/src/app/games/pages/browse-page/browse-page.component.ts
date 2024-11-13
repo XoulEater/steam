2d8cfb1bd7e9d51ee2e773f929c0d75291a2283f
XoulEater <ncqueescribir@gmail.com>
2024-11-13 14:17:33 -0600

    Add pagination functionality to BrowsePage and create shared pagination component

--- frontend/src/app/games/pages/browse-page/browse-page.component.ts
+++ frontend/src/app/games/pages/browse-page/browse-page.component.ts
@@ -1,18 +1,20 @@
 import { GamesService } from './../../services/games.service';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Category } from '../../interfaces/games.interfaces';
+import { Category, Game } from '../../interfaces/games.interfaces';
 import { FilterOptionsComponent } from '../../component/filter-options/filter-options.component';
+import { PaginationComponent } from '../../../shared/components/pagination/pagination.component';
 
 @Component({
   selector: 'app-browse-page',
   standalone: true,
-  imports: [CommonModule, FilterOptionsComponent],
+  imports: [CommonModule, FilterOptionsComponent, PaginationComponent],
   templateUrl: './browse-page.component.html',
   styles: ``,
 })
 export class BrowsePageComponent {
   public categories: Category[] = [];
+  public games: Game[] = [];
   public developers: string[] = ['Developer 1', 'Developer 2', 'Developer 3'];
 
   constructor(private gamesService: GamesService) {}
@@ -21,6 +23,23 @@ export class BrowsePageComponent {
     this.gamesService.getCategories().subscribe((categories) => {
       this.categories = categories;
     });
+    this.gamesService.getGames().subscribe((games) => {
+      this.games = games;
+    });
+
     // TODO: Get developers from the service
+  }
+
+  currentPage = 1;
+  itemsPerPage = 5;
+
+  get paginatedGames() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.games.slice(startIndex, endIndex);
+  }
+
+  onPageChange(page: number) {
+    this.currentPage = page;
   }
 }
---
+++ frontend/src/app/shared/components/pagination/pagination.component.ts
@@ -0,0 +1,67 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'shared-pagination',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagination.component.html',
+  styles: ``,
+})
+export class PaginationComponent {
+  @Input() totalItems = 100;
+  @Input() itemsPerPage = 10;
+  @Output() pageChange = new EventEmitter<number>();
+
+  totalPages!: number;
+  currentPage = 1;
+  pages: number[] = [];
+
+  ngOnInit() {
+    this.totalPages = Math.ceil(this.totalItems / this.itemsPerPage);
+    this.updatePages();
+  }
+
+  updatePages() {
+    const pageNumbers: number[] = [];
+
+    // Mostrar las primeras 5 páginas o las 5 más cercanas a la página actual
+    const start = Math.max(1, this.currentPage - 2);
+    const end = Math.min(this.totalPages, this.currentPage + 2);
+
+    console.log(start, end);
+
+    // Agregar los números de las páginas visibles
+    for (let i = start; i <= end; i++) {
+      pageNumbers.push(i);
+    }
+
+    // Añadir el último número de página, si no está ya en la lista
+    if (this.totalPages > end) {
+      if (end + 1 < this.totalPages) {
+        pageNumbers.push(-1); // -1 indica el "..." en el template
+      }
+      pageNumbers.push(this.totalPages);
+    }
+
+    this.pages = pageNumbers;
+  }
+
+  goToPage(page: number) {
+    if (page === -1) return; // Ignorar clics en el "..."
+    this.currentPage = page;
+    this.pageChange.emit(this.currentPage);
+    this.updatePages();
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.goToPage(this.currentPage - 1);
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.goToPage(this.currentPage + 1);
+    }
+  }
+}
