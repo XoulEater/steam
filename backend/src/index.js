841f758ea918decd99730fc418cfcb6dc873dfd6
FranVQ09 <franciscovq@estudiantec.cr>
2024-11-18 15:01:38 -0600

    Nuevo index para render

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-//Routes constants
+// Routes constants
 const userRoutes = require('./routes/users');
 const adminRoutes = require('./routes/adminLogistic');
 const userAuthRoutes = require('./routes/usersAuth');
@@ -14,31 +14,39 @@ const app = express();
 // Usa el puerto asignado por Render, si está disponible, o 10000 como fallback
 const port = process.env.PORT || 10000;
 
-//Middleware
-app.use(express.json());
+// Configurar CORS con opciones específicas
+const corsOptions = {
+  origin: process.env.CLIENT_URL || '*',  // Reemplaza CLIENT_URL con la URL de tu frontend en producción
+  allowedHeaders: ['Content-Type', 'Authorization'],  // Permitir 'Authorization' para JWT
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],  // Métodos permitidos
+};
+app.use(cors(corsOptions));  // Aplica la configuración de CORS
+
+// Middleware
+app.use(express.json()); // Parseo de JSON
 app.use(cors()); // Para permitir peticiones de diferentes dominios
 
-//Import Routes
+// Import Routes
 app.use('/user', userRoutes);
 app.use('/admin-logistic', adminRoutes);
 app.use('/auth', userAuthRoutes);
 app.use('/products', productsRoutes);
 
-//Routes
+// Routes
 app.get('/', (req, res) => {
-    res.send('Welcome to the backend');
+  res.send('Welcome to the backend');
 });
 
-//Connect to MongoDB
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((error) => {
-        console.log('Error connecting to MongoDB', error);
-    });
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB', error);
+  });
 
 // Solo una llamada a app.listen
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
